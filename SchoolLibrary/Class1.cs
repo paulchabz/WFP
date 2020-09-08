cd642d51786cd9e300b9fd5e6fccaccafbf355dc
paulchabz <paulwchabz@gmail.com>
2020-09-08 22:08:50 +0100

    first commit

---
+++ SchoolLibrary/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class Class1
+    {
+        
+        public string Name { get; set; } // uses encapsulation
+        public string SchoolAddress { get; set; }
+        public string SchoolCity { get; set; }
+        public string SchoolState { get; set; }
+        public string SchoolZip { get; set; }
+        public string PhoneNumber { get; set; }
+        private string _twitterAddress;
+        public string TwitterAddress
+        {
+            // starts with @
+            get { return _twitterAddress; }
+            set
+            {
+                if (value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                } else
+                {
+                    throw new Exception("Your twitter address must begin with an @");
+                }
+            }
+
+        }
+        // constructor 
+        public Class1()
+        {
+            Name = "Rhodes Park High School";
+            PhoneNumber = "555-5678-989";
+            SchoolCity = "Portland";
+        }
+        // overloaded constructor 
+        public Class1(string SchoolName, string SchoolPhoneNumber)
+        {
+            Name = SchoolName;
+            PhoneNumber = SchoolPhoneNumber;
+        }
+        // Methods functions 
+        //public float AverageThreeScores(float a, float b, float c)
+        //{
+        //    var result = a + b + c / 3;
+        //    return result;
+        //}
+
+        public float AverageThreeScores(float a, float b, float c) => (a + b + c) / 3;
+
+        public int AverageThreeScores(int a, int b, int c)
+        {
+            var result = a + b + c / 3;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(this.Name);
+            sb.AppendLine(this.SchoolAddress);
+            sb.AppendLine(this.SchoolCity);
+            sb.Append(", ");
+            sb.Append(this.SchoolState);
+            sb.Append(", ");
+            sb.Append(this.SchoolZip);
+            return sb.ToString();
+        }
+    }
+}
---
+++ SchoolsFormApp/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolLibrary;
+using System.Windows.Forms;
+
+namespace SchoolsFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // event handler 
+            var testSchool = new SchoolLibrary.Class1();
+            testSchool.Name = "Central City High";
+        }
+    }
+}
